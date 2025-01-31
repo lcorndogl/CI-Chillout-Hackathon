18bd7101e91ce30a092281d7d1320e54e619c37f
Michael Cornall <mik.reds.forever@gmail.com>
2025-01-31 09:59:58 +0000

    add reaction, leaderboard page to view, urls.py and direct them to newly added leaderboard, reaction html files

--- reactions/urls.py
+++ reactions/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('reaction/leaderboard', views.leaderboard, name='leaderboard'),
+    path('reaction', views.reaction, name='reaction'),
 
 ]
