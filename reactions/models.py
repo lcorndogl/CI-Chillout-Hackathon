be7d2666e726663a147378e12152351e576d4645
Michael Cornall <mik.reds.forever@gmail.com>
2025-02-02 15:22:01 +0000

    Add model, form for loading scores into DB

--- reactions/models.py
+++ reactions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Scores(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    score = models.FloatField()
+    
