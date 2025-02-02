fa2effcbe606276470ce6baa41b442b3e4af4572
Michael Cornall <mik.reds.forever@gmail.com>
2025-02-02 15:39:44 +0000

    rename scores model to score

--- reactions/admin.py
+++ reactions/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Scores
+from .models import Score
 
 
 # Register your models here.
-@admin.register(Scores)
+@admin.register(Score)
 class ScoresAdmin(admin.ModelAdmin):
     list_display = ('user', 'score', 'created_on', 'updated_on')
     list_filter = ('created_on', 'updated_on')
--- reactions/models.py
+++ reactions/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Scores(models.Model):
+class Score(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
