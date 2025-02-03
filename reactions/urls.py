c787d1aba4d397a694c710bbe1d93ed388c1be75
Michael Cornall <mik.reds.forever@gmail.com>
2025-02-03 09:50:51 +0000

    Create profile page, deploy to test why profile url isn't working as intended

--- reactions/urls.py
+++ reactions/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('profile/', views.profile, name='profile'),
+    path('delete-score/<int:score_id>/', views.delete_score, name='delete_score'),
     path('', views.home, name='home'),
     path('reaction/leaderboard', views.leaderboard, name='leaderboard'),
     path('reaction', views.reaction, name='reaction'),
-
+    path('manage', views.reaction, name='manage'),
+    path('save-reaction-time/', views.save_reaction_time, name='save_reaction_time'),
 ]
